3c19159b4cff5a4ca0e59cb52534b812316820c1
thibautloiseau <thibaut.loiseau@gmail.com>
2025-06-30 23:30:58 +0200

    Add experiment with no norm layers

--- run_single_gpu.sh
+++ run_single_gpu.sh
@@ -2,6 +2,6 @@
 
 # Run training with Hydra configuration
 torchrun --standalone --nproc_per_node=1 train.py \
-    experiment=pomgpt_baseline \
+    experiment=pomngpt \
     training.batch_size=24 \
     training.accumulation=16
