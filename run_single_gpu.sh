5e9b1111474730aed8122593d9cb5d97fe3226b6
thibautloiseau <thibaut.loiseau@gmail.com>
2025-07-01 15:56:29 +0200

    jz exp for matrix norm

--- run_single_gpu.sh
+++ run_single_gpu.sh
@@ -2,6 +2,6 @@
 
 # Run training with Hydra configuration
 torchrun --standalone --nproc_per_node=1 train.py \
-    experiment=pomngpt_justnorm \
+    experiment=pomngpt_justnorm_matrix \
     training.batch_size=24 \
     training.accumulation=16
