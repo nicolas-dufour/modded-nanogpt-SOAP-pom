01c59396e512cffa31c4d7d539f2d54e94f1141c
thibautloiseau <thibaut.loiseau@gmail.com>
2025-07-02 15:56:07 +0200

    jz exp pomngpt full

---
+++ jz/pomngpt_full.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=pomngpt_full
+#SBATCH --output=logs/pomngpt_full/%j.out
+#SBATCH --error=logs/pomngpt_full/%j.err
+#SBATCH --account=wre@h100
+#SBATCH --constraint=h100
+#SBATCH --qos=qos_gpu_h100-dev
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=96
+#SBATCH --time=02:00:00
+#SBATCH --hint=nomultithread
+
+module purge
+
+module load arch/h100
+module load miniforge/24.9.0
+conda activate pom
+
+# Run the script
+cd $WORK/modded-nanogpt-SOAP-pom
+
+export WANDB_MODE=offline
+
+set -x
+srun torchrun --standalone --nproc_per_node=4 train.py \
+     experiment=pomngpt_full
