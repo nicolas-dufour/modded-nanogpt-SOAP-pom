4efd8d95959f88166d0c6ecbc9dd34d0bd2a3a28
thibautloiseau <thibaut.loiseau@gmail.com>
2025-07-04 16:42:12 +0200

    add logs for baseline

--- run_single_gpu.sh
+++ run_single_gpu.sh
@@ -2,7 +2,7 @@
 
 # Run training with Hydra configuration
 torchrun --standalone --nproc_per_node=1 train.py \
-    experiment=pomgpt_baseline \
+    experiment=pomngpt_full \
     training.batch_size=8 \
     training.accumulation=12 \
     model.gpt.log_stats=1 \
